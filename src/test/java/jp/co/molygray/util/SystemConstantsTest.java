432e03844aa9b5a0e5d168bc435ab5a79fa69ab6
mori-akira <kgrmrak@yahoo.co.jp>
2023-05-04 22:45:49 +0900

    update

---
+++ src/test/java/jp/co/molygray/util/SystemConstantsTest.java
@@ -0,0 +1,40 @@
+package jp.co.molygray.util;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.commons.lang3.reflect.FieldUtils;
+import org.junit.jupiter.api.Test;
+
+/**
+ * {@link SystemConstants}のテストクラス
+ *
+ * @author Moriaki Kogure
+ * @version 0.0.1
+ */
+public class SystemConstantsTest {
+
+  /**
+   * {@link SystemConstants#getConstants()}のテストメソッド
+   *
+   * @throws Exception 例外発生時
+   */
+  @Test
+  public void getConstantsTest()
+      throws Exception {
+    SystemConstants systemConstants = new SystemConstants();
+    Map<String, String> constants = new HashMap<>() {
+
+      {
+        put("k1", "v1");
+        put("k3", "v3");
+      }
+    };
+    FieldUtils.writeField(SystemConstants.class.getDeclaredField("constants"), systemConstants,
+        constants, true);
+    assertEquals("v1", systemConstants.getConstants("k1"));
+    assertEquals(null, systemConstants.getConstants("k2"));
+    assertEquals("v3", systemConstants.getConstantsWithDefault("k3", "V3"));
+    assertEquals("V4", systemConstants.getConstantsWithDefault("k4", "V4"));
+  }
+}
