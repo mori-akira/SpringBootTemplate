15773bcb2e019d28e8af63a7a9ad9f9be5a9f99b
mori-akira <kgrmrak@yahoo.co.jp>
2023-05-14 11:38:13 +0900

    refactor

--- src/main/java/jp/co/molygray/annotation/WithExclusiveCheck.java
+++ src/main/java/jp/co/molygray/annotation/WithExclusiveCheck.java
@@ -5,12 +5,14 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
+import jp.co.molygray.advice.ExclusiveCheckAdvice;
 
 /**
  * 排他チェックを行うことを示すアノテーション
  *
  * @author Moriaki Kogure
  * @version 0.0.1
+ * @see ExclusiveCheckAdvice
  */
 @Target({METHOD})
 @Retention(RUNTIME)
